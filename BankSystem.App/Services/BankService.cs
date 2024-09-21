0c823902bf3f657d7300e26b0e9faaa4ca2bcf6a
Kapebah <sh1n0sh1@mail.ru>
2024-09-21 22:56:08 +0300

    type conversion practice is done

---
+++ BankSystem.App/Services/BankService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankSystem.Domain.Models;
+
+namespace BankSystem.App.Services
+{
+    public class BankService
+    {
+        public static int calculateSalary(int bankProfit, int expenses, int ownerNumber) 
+            => (bankProfit - expenses) / ownerNumber;
+        
+
+        public static Employee convertClientToEmployee(Client client)
+            => new Employee
+            {
+                Name = client.Name,
+                Age = client.Age,
+                PassportDetails = client.PassportDetails,
+                PhoneNumber = client.PhoneNumber
+            };
+        
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,7 +1,16 @@
 ﻿using BankSystem.Domain.Models;
+using BankSystem.App.Services;
 using static System.Console;
 
-Employee employee = new Employee { Name = "Felix", Age = 27, PassportDetails = "1234-567890", Contruct = "Contruct isn't confirm yet"};
+//Practice 1
+
+Employee employee = new Employee
+{
+    Name = "Felix",
+    Age = 27,
+    PassportDetails = "1234-567890",
+    Contruct = "Contruct isn't confirm yet"
+};
 Currency currency = new Currency { Type = "RUB" };
 
 void updateContruct (Employee employee)
@@ -17,10 +26,30 @@ void updateCurrency (Currency currency)
 updateContruct(employee);
 updateCurrency(currency);
 
+//Значение в employee.Contruct поменялось, а в currency.Type нет,
+//т.к. при передаче объекта класса мы ссылаемся на одну и ту же ссылку,
+//вследствие чего у нас обновляются данные и вне метода,
+//а при передаче структуры у нас создается копия (новый объект),
+//и изменения в копии не влияют на оригинал
 WriteLine($"{employee.Name}'s {employee.Contruct}");
 WriteLine($"{currency.Type}");
 
+//Practice 2
+
+Client client = new Client
+{
+    Name = "Eugene",
+    Age = 23,
+    PhoneNumber = "123456789",
+    PassportDetails = "1234-567890"
+};
+
+List<Employee> employees = new List<Employee>()
+{
+    employee,
+    BankService.convertClientToEmployee(client)
+};
+
+WriteLine($"Salary: {BankService.calculateSalary(60000, 20000, employees.Count)}");
+
 ReadKey();
-
-
-
